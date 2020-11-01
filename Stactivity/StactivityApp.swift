bfdf1139fe94836167376e96f44afa8ea8d96ddb
Yannik Zimmermann <yannik.z@me.com>
2020-11-01 15:55:48 +0100

    Initial Commit

---
+++ Stactivity/StactivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  StactivityApp.swift
+//  Stactivity
+//
+//  Created by Yannik Zimmermann on 01.11.20.
+//
+
+import SwiftUI
+
+@main
+struct StactivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
